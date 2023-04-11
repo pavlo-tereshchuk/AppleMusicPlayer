43a95aa3ab786502ccd5475d684086cacdd646dc
Pavlo Tereshchuk <pavlo.tereshchuk@edag.com>
2023-04-11 11:09:36 +0200

    - added class for audio player

---
+++ tabBar/tabBar/Model/AudioPlayer.swift
@@ -0,0 +1,60 @@
+//
+//  AudioPlayer.swift
+//  tabBar
+//
+//  Created by Pavlo Tereshchuk on 07.04.23.
+//
+
+import Foundation
+import AVFoundation
+import SwiftUI
+
+class AudioPlayer {
+    var audioPlayer: AVAudioPlayer?
+    
+    
+    func playSong(_ songName: String) {
+        let filePath = Bundle.main.path(forResource: songName, ofType: "mp3")
+        
+        // Create a URL object from the file path
+        let fileURL = URL(fileURLWithPath: filePath!)
+        
+        do {
+            // Initialize the audio player with the song file
+            audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
+            
+            // Play the song
+            audioPlayer?.play()
+            
+            // Rewind the song to the beginning
+            audioPlayer?.currentTime = 0
+            
+        } catch {
+            // If there was an error initializing the audio player, print the error
+            print(error.localizedDescription)
+        }
+    }
+}
+
+class Song {
+    let name: String
+    let url: URL
+    let image: UIImage
+    
+    init(name: String, image: UIImage) {
+        self.name = name
+        let filePath = Bundle.main.path(forResource: name, ofType: "mp3")
+        self.url = URL(fileURLWithPath: filePath!)
+        extractSongData()
+    }
+    
+    func extractSongData() {
+        let asset = AVURLAsset(url: url)
+        let generator = AVAssetImageGenerator(asset: asset)
+        generator.appliesPreferredTrackTransform = true
+        let timestamp = CMTime(seconds: 5, preferredTimescale: 1)
+        let imageRef = try? generator.copyCGImage(at: timestamp, actualTime: nil)
+        self.image = imageRef == nil ? UIImage() : UIImage(cgImage: imageRef!)
+    }
+}
+
