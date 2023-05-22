9927f975ded0f7efc75dc3040e96d1e0d9df2bde
Pavlo Tereshchuk <pavlo.tereshchuk@edag.com>
2023-05-22 14:40:47 +0200

    - first version of SongList

--- tabBar/tabBar/View/Components/SongsList.swift
+++ tabBar/tabBar/View/Components/SongsList.swift
@@ -32,26 +32,52 @@ struct SongsList: View {
     var body: some View {
             ZStack(alignment: .top) {
                 List {
-                    ForEach(songs) { song in
-                        SongRow(song: song)
+                    Section(header:
+                        sectionHistory()
+                    ) {
+                        ForEach(songs) { song in
+                            SongRow(song: song)
+                                .frame(maxWidth: .infinity)
+                                .listRowInsets(EdgeInsets())
+                                .padding(.bottom, 10)
+                                .background(.clear)
+                        }
+                        .listRowBackground(Color.clear)
+                        .listRowSeparator(.hidden)
+                    }
+
+                    Section {
+                        SongRow(song: currentSong)
                             .frame(maxWidth: .infinity)
                             .listRowInsets(EdgeInsets())
                             .padding(.bottom, 10)
                             .background(.clear)
-                            .onTapGesture {
-                                print("TAP")
-                            }
-                            .onLongPressGesture(minimumDuration: 1) {
-                                self.pressedRowID = song.id
-                                print("Long")
-                            }
-                        
+                            .listRowBackground(Color.clear)
+                            .listRowSeparator(.hidden)
                     }
-                    .onMove(perform: moveRow)
-                    .listRowBackground(Color.clear)
-                    .listRowSeparator(.hidden)
+
+                    Section(header: sectionQueue()) {
+                        ForEach(songs) { song in
+                            SongRow(song: song)
+                                .frame(maxWidth: .infinity)
+                                .listRowInsets(EdgeInsets())
+                                .padding(.bottom, 10)
+                                .background(.clear)
+                                .onTapGesture {
+                                    print("TAP")
+                                }
+                                .onLongPressGesture(minimumDuration: 1) {
+                                    self.pressedRowID = song.id
+                                    print("Long")
+                                }
+
+                        }
+                        .onMove(perform: moveRow)
+                        .listRowBackground(Color.clear)
+                        .listRowSeparator(.hidden)
+                    }
                 }
-                .padding(.top, 60)
+                .padding(.horizontal, 5)
                 .listStyle(PlainListStyle())
                 .environment(\.editMode, .constant(.active))
                 .scrollIndicators(.hidden)
@@ -62,58 +88,50 @@ struct SongsList: View {
                 .onDisappear {
                     UIScrollView.appearance().bounces = true
                 }
-                
-                
-                HStack(spacing: 15) {
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("In queue")
-                            .font(.body)
-                            .foregroundColor(.white)
-                        Text("Autoplay similar music")
-                            .font(.caption)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(.ultraThickMaterial)
-                            .opacity(0.65)
-                    }
-                    
-                    Spacer()
-                    HStack(spacing: 15) {
-                        Button {
-                            withAnimation(.easeInOut(duration: 0.05)) {
-                                shuffleButton.toggle()
-                            }
-                        } label: {
-                            customButtonLabel(imageName: "shuffle", toggleButton: shuffleButton, paddingEdges: .vertical, paddingLength: 1, toggleColor: currentSong.averageColor)
-                                .scaleEffect(1.3)
-                            
-                        }
-                        
-                        Button {
-                            withAnimation(.easeInOut(duration: 0.05)) {
-                                repeatButton.toggle()
-                            }
-                        } label: {
-                            customButtonLabel(imageName: "repeat", toggleButton: repeatButton, paddingEdges: .vertical, paddingLength: 1, toggleColor: currentSong.averageColor)
-                                .scaleEffect(1.3)
-                        }
-                        
-                        Button {
-                            withAnimation(.easeInOut(duration: 0.05)) {
-                                infinityButton.toggle()
-                            }
-                        } label: {
-                            customButtonLabel(imageName: "infinity", toggleButton: infinityButton, paddingEdges: .vertical, paddingLength: 1, toggleColor: currentSong.averageColor)
-                                .scaleEffect(1.3)
-                        }
-                    }
-                    .padding(.vertical)
-                }
-                .frame(maxWidth: .infinity)
-                .listRowBackground(Color.clear)
-                .listRowSeparator(.hidden)
-                .padding(.horizontal)
+
+
+
             }
     }
+    
+//    var body: some View {
+//        ScrollView(showsIndicators: false) {
+//            LazyVStack(alignment: .leading, spacing: 0, pinnedViews: [.sectionHeaders]) {
+//                Section {
+//                    ForEach(songs) { song in
+//                        SongRow(song: song)
+//                            .frame(maxWidth: .infinity)
+//                            .background(.clear)
+//                    }
+//                } header: {
+//                    sectionHistory()
+//                }
+//
+//                Section {
+//                    SongRow(song: currentSong)
+//                }
+//
+//                Section {
+//                    ForEach(songs) { song in
+//                        SongRow(song: song)
+//                            .frame(maxWidth: .infinity)
+//                            .background(.clear)
+//                    }
+//                } header: {
+//                    sectionQueue()
+//                }
+//            }
+//            .padding(.horizontal, 5)
+//            .environment(\.editMode, .constant(.active))
+//            .scrollIndicators(.hidden)
+//            .onAppear {
+//                UIScrollView.appearance().bounces = false
+//            }
+//            .onDisappear {
+//                UIScrollView.appearance().bounces = true
+//            }
+//        }
+//    }
     
     @ViewBuilder
     func customButtonLabel(imageName: String, toggleButton: Bool, paddingEdges: Edge.Set = .all, paddingLength: CGFloat = 0, toggleColor: Color) -> some View {
@@ -137,6 +155,84 @@ struct SongsList: View {
         .scaleEffect(0.8)
     }
     
+    @ViewBuilder
+    func sectionHistory() -> some View {
+        HStack {
+            Text("History")
+                .font(.body)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Text("Clear")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.ultraThickMaterial)
+                    .opacity(0.65)
+            }
+        }
+        .frame(height: 60)
+        .listRowBackground(Color.clear)
+        .listRowInsets(EdgeInsets())
+    }
+    
+    @ViewBuilder
+    func sectionQueue() -> some View {
+        HStack(spacing: 15) {
+            VStack(alignment: .leading, spacing: 5) {
+                Text("In queue")
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Text("Autoplay similar music")
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.ultraThickMaterial)
+                    .opacity(0.65)
+            }
+
+            Spacer()
+            HStack(spacing: 15) {
+                Button {
+                    withAnimation(.easeInOut(duration: 0.05)) {
+                        shuffleButton.toggle()
+                    }
+                } label: {
+                    customButtonLabel(imageName: "shuffle", toggleButton: shuffleButton, paddingEdges: .vertical, paddingLength: 1, toggleColor: currentSong.averageColor)
+                        .scaleEffect(1.3)
+
+                }
+
+                Button {
+                    withAnimation(.easeInOut(duration: 0.05)) {
+                        repeatButton.toggle()
+                    }
+                } label: {
+                    customButtonLabel(imageName: "repeat", toggleButton: repeatButton, paddingEdges: .vertical, paddingLength: 1, toggleColor: currentSong.averageColor)
+                        .scaleEffect(1.3)
+                }
+
+                Button {
+                    withAnimation(.easeInOut(duration: 0.05)) {
+                        infinityButton.toggle()
+                    }
+                } label: {
+                    customButtonLabel(imageName: "infinity", toggleButton: infinityButton, paddingEdges: .vertical, paddingLength: 1, toggleColor: currentSong.averageColor)
+                        .scaleEffect(1.3)
+                }
+            }
+            .padding(.vertical)
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: 60)
+        .listRowBackground(Color.clear)
+        .listRowInsets(EdgeInsets())
+    }
+    
     private func moveRow(source: IndexSet, destination: Int){
         songs.move(fromOffsets: source, toOffset: destination)
     }
@@ -146,6 +242,8 @@ struct SongsList_Previews: PreviewProvider {
     
     static let curSong = Song(name: "Juice Jones")
     @State static var songs = [Song(name: "Mermaids"), curSong,
+                               Song(name: "Daft_Punk_GLBTM"), Song(name: "Mermaids"), curSong,
+                               Song(name: "Daft_Punk_GLBTM"), Song(name: "Mermaids"), curSong,
                                Song(name: "Daft_Punk_GLBTM")]
 
     static var previews: some View {
